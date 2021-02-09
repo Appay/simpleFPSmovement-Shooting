bed67c82040178feeac1155e1f1e590525922087
Alpay Aygün <alpayaygun40@gmail.com>
2021-02-09 11:44:53 +0300

    fpsMovement-Shooting
    unity fps character movement and killing enemy

---
+++ fpshooter/Assets/MovementScript/MouseLook.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // sağa sola yukarı aşağı bakma
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;//maus hareketleri ile çevreye baktırma
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);// clamp ile yukarı aşağı bakış açısına sınır getirdik arkasına direk bakamaz vb
+
+        transform.localRotation = Quaternion.Euler(xRotation,0f,0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+        //-- sağa sola yukarı aşağı bakma
+
+    }
+}
---
+++ fpshooter/Assets/ShootnKill/Shooting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject bulletSpawn;
+    public float fireRate;
+
+    private Transform _bullet;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    public void Fire()
+    {
+      _bullet =  Instantiate(bullet.transform, bulletSpawn.transform.position, Quaternion.identity);
+
+    }
+
+}
+
+
